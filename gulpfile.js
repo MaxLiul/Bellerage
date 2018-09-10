05ac627d7fdd8589c63cd9581efb85e45440a221
Liul Maxim <m.liul@bellerage.com>
2018-09-10 20:54:14 +0300

    Webpack and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var imagemin = require('gulp-imagemin')
+gulp.task('sass', function () {
 
-gulp.task('sass', function(){
-    
-    return gulp.src("app/sass/main.sass")
-    .pipe(sass())
-    .pipe(gulp.dest("app/css"))
-    .pipe(browserSync.reload({stream: true}));
+    return gulp.src("app/sass/**/*.sass")
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-gulp.task('browser-sync',  function(){
+gulp.task('img', function () {
+
+    return gulp.src("app/img/*.png")
+        .pipe(imagemin())
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+gulp.task('browser-sync', function () {
     browserSync({
         server: {
             baseDir: 'app'
@@ -22,11 +33,12 @@ gulp.task('browser-sync',  function(){
 
 
 
-gulp.task('watch', gulp.series('sass', 'browser-sync'), function(){
-     gulp.watch('app/css/main.css', gulp.series('sass'));
-     gulp.watch('app/*.html', browserSync.reload);
-     gulp.watch('app/js/**/*.js', browserSync.reload);
-    
+gulp.task('watch', gulp.series('sass', 'img', 'browser-sync'), function () {
+    gulp.watch('app/css/main.css', gulp.series('sass'));
+    gulp.watch('app/css/img.png', gulp.series('sass'));
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+
 });
 
 //gulp.task('build', gulp.series('sass'), gulp.parallel('watch', 'browser-sync'));
