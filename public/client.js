9ac5081a20d8c973efeddb69ee3153306bd3d93d
Liul Maxim <m.liul@bellerage.com>
2018-09-17 13:25:46 +0300

    post-method

--- public/client.js
+++ public/client.js
@@ -1,24 +1,58 @@
 var submitButton = document.getElementById('submit_button');
-var formData = new FormData(document.getElementById('form'));
+// var formData = new FormData(document.getElementById('form'));
 submitButton.addEventListener('click', sendInput);
-
 var xhr = new XMLHttpRequest();
 
-function sendInput(){
+function sendInput() {
   var input = document.getElementById('input');
-  var params = `${input.name}=` + encodeURIComponent(input.value);
-  
-  xhr.open('GET', 'http://localhost:3000/send?' + params, true); // инициализация соединения
-  xhr.send();
-  xhr.onreadystatechange = function showServerAnswer() {
-    if(xhr.readyState === xhr.DONE && xhr.status === 200){
+  var jsonBody = JSON.stringify({
+    [input.name]: input.value
+  })
+
+  xhr.open('POST', 'http://localhost:3000/send', true); // инициализация соединения
+  xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  xhr.send(jsonBody);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === xhr.DONE && xhr.status === 200) {
       var serverResponse = document.getElementById('response');
-      serverResponse.innerText = xhr.response;
+      serverResponse.innerText = JSON.parse(xhr.response).modifiedText;
     }
   }
-  
 }
 
 
+// post-запрос
+// function sendInput(){
+//   var input = document.getElementById('input');
+//   var body = `${input.name}=` + encodeURIComponent(input.value);
+
+//   xhr.open('Post', 'http://localhost:3000/send', true); // инициализация соединения
+//   xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+//   xhr.send(body);
+//   xhr.onreadystatechange = function(){
+//     if(xhr.readyState === xhr.DONE && xhr.status === 200){
+//       var serverResponse = document.getElementById('response');
+//       serverResponse.innerText = xhr.response;
+//   }
+//   }
+// }
 
 
+
+
+
+// get-запрос
+// function sendInput(){
+//   var input = document.getElementById('input');
+//   var params = `${input.name}=` + encodeURIComponent(input.value);
+
+//   xhr.open('GET', 'http://localhost:3000/send?' + params, true); // инициализация соединения
+//   xhr.send();
+//   xhr.onreadystatechange = function showServerAnswer() {
+//     if(xhr.readyState === xhr.DONE && xhr.status === 200){
+//       var serverResponse = document.getElementById('response');
+//       serverResponse.innerText = xhr.response;
+//     }
+//   }
+
+// }
--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 var express = require("express");
 var app = express();
+var parser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(parser());
 
-app.get('/send', function (req, res) {
-  console.log(req.query.input_text);
-  res.send(req.query.input_text); 
+app.post('/send', function (req, res) {
+ // console.log('text', res);
+ var response = {
+   modifiedText : `Вы ввели ${req.body.input_text}` 
+ };
+  //res.set('Content-type', 'application/json; charset=utf-8')
+  res.send(JSON.stringify(response)); 
    
 });
+// app.get('/send', function (req, res) {
+//   console.log(req.query.input_text);
+//   res.send(req.query.input_text); 
+   
+// });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
